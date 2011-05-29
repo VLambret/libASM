cb33b0fd943d486a6bc4afd7f5686450cfe7426f
Victor Lambret <lambret.victor@gmail.com>
2011-05-30 01:46:52 +0200

    Try to add new .set rules to parser.

---
+++ src/examples/test/test_01a.s
@@ -0,0 +1,34 @@
+	.file	1 "test_01a.c"
+	.section .mdebug.abi32
+	.previous
+	.gnu_attribute 4, 1
+	.abicalls
+	.text
+	.align	2
+	.globl	main
+	.set	nomips16
+	.ent	main
+	.type	main, @function
+main:
+	.frame	$fp,24,$31		# vars= 8, regs= 1/0, args= 0, gp= 8
+	.mask	0x40000000,-4
+	.fmask	0x00000000,0
+	.set	noreorder
+	.set	nomacro
+	addiu	$sp,$sp,-24
+	sw	$fp,20($sp)
+	move	$fp,$sp
+	li	$2,2			# 0x2
+	sw	$2,8($fp)
+	lw	$2,8($fp)
+	move	$sp,$fp
+	lw	$fp,20($sp)
+	addiu	$sp,$sp,24
+	j	$31
+	nop
+
+	.set	macro
+	.set	reorder
+	.end	main
+	.size	main, .-main
+	.ident	"GCC: (GNU) 4.5.2"
---
+++ src/mains/test_01a.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <OPLabel.h>
+#include <Program.h>
+#include <OPExpression.h>
+#include <OPRegister.h>
+#include <OPImmediate.h>
+
+
+int main(){
+	Program p2("src/examples/test/test_01a.s") ;
+	p2.inFile("tmp/test_01a.s") ;
+}
