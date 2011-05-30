68c35d0a9c7fbe8dbec5e70f065fc11cea2a4b69
Victor Lambret <lambret.victor@gmail.com>
2011-05-30 14:44:31 +0200

    add some new files to test gcc mips issues

---
+++ src/mains/test_01b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <OPLabel.h>
+#include <Program.h>
+#include <OPExpression.h>
+#include <OPRegister.h>
+#include <OPImmediate.h>
+
+
+int main(){
+	Program p2("src/examples/test/test_01b.s") ;
+	p2.inFile("tmp/test_01b.s") ;
+}
